5b5bf20c220b5325dd3d7af4a28223bde071bb2f
CraigJensen <cmjensen82@gmail.com>
2022-02-12 13:42:07 -0600

    added more libraries, express-session, path, sequelize, mysql2

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESS_PW,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
