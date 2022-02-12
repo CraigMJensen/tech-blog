5bb2ce82d630f9144d3fd4b6cefce7f2230e8f4a
CraigJensen <cmjensen82@gmail.com>
2022-02-12 13:24:46 -0600

    added schema.sql file, database connects

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
-app.use(routes);
+// app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
