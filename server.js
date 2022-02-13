e1c2e4106a99994d1985a956cc881ac5d0d207a3
CraigJensen <cmjensen82@gmail.com>
2022-02-13 08:04:19 -0600

    added routes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
-// app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
